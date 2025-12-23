1f41d76090fdff740aceea542c8f6c6948df0943
Sanket Shewale <sanketshewale2001@gmail.com>
2025-12-23 23:03:24 +0530

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE employee (
+    Education VARCHAR(50),
+    JoiningYear INT,
+    City VARCHAR(50),
+    PaymentTier INT,
+    Age INT,
+    Gender VARCHAR(10),
+    EverBenched VARCHAR(5),
+    ExperienceInCurrentDomain INT,
+    LeaveOrNot INT
+);
